a986ec7aefdb73b2abd4767f911492c23afd46c1
jimin <010709min@ewhain.net>
2024-11-17 13:19:56 +0900

    [feat] mypage model

--- capstone/mypage/models.py
+++ capstone/mypage/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from accounts.models import User
 
 # Create your models here.
+class MyPage(models.Model):
+  name = models.CharField(max_length=50)
+  background_photo = models.ImageField(upload_to='backgrounds/')
+  color = models.CharField(max_length=100)
+  typography = models.CharField(max_length=300)
+  user = models.ForeignKey(User)
