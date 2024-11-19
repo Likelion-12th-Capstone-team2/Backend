1c7c192f7aec90625873b5bd81296aed7734cbf5
jimin <010709min@ewhain.net>
2024-11-19 12:52:30 +0900

    [feat] mypage serializer 생성

---
+++ capstone/mypage/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.shortcuts import get_object_or_404
+from .models import *
+
+class MyPageSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = MyPage
+    fields = ['id', 'name', 'background_photo', 'color', 'typography', 'user']
