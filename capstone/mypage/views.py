ada7128bb8c9b7c109371838f1c68ace5cc20f08
jimin <010709min@ewhain.net>
2024-11-21 14:42:35 +0900

    [fix] category 생성 로직 수정
    카테고리 수 7개 이상 400, 같은 카테고리명 400 처리

--- capstone/mypage/views.py
+++ capstone/mypage/views.py
@@ -20,7 +20,7 @@ class MypageView(views.APIView):
     # 이미 mypage가 생성된 유저인 경우 400 오류
     if MyPage.objects.filter(user=request.user).exists():
       return Response({"error": "이미 mypage가 생성된 유저입니다."}, status=HTTP_400_BAD_REQUEST)
-    
+
     serializer = MyPageSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save(user=request.user)
@@ -69,6 +69,11 @@ class CategoryView(views.APIView):
     if not request.user.is_authenticated:
       return Response({"error": "로그인 후 카테고리를 생성할 수 있습니다."}, status=HTTP_400_BAD_REQUEST)
     
+    if Category.objects.filter(user_id=request.user.id).count > 7:
+      return Response({"error": "카테고리는 최대 7개까지만 설정 가능합니다."}, status=HTTP_400_BAD_REQUEST)
+    
+    
+
     serializer = CategorySerializer(data=request.data)
     if serializer.is_valid():
       serializer.save(user=request.user)
