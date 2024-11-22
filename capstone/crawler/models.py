d16474dab509da4f2a065e0d5e9050ee67e26af8
Minseo Park <qkralstj0926@gmail.com>
2024-11-22 21:27:48 +0900

    [feat] add crawler/urls.py

--- capstone/crawler/models.py
+++ capstone/crawler/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+from django.urls import path
+from . import views
 
-# Create your models here.
+urlpatterns = [
+    path('crawl/', views.crawl_product, name='crawl_product'),
+]
