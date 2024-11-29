d86d0e931c80a60dcc4ac0e04707f13fb54d153e
minsihihi <qkralstj@gmail.com>
2024-11-29 17:31:55 +0900

    00

--- capstone/urls.py
+++ capstone/urls.py
@@ -9,11 +9,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('mypages/', include('mypage.urls')),
     path('wish/', include('wish.urls')),
-<<<<<<< HEAD
     path('alarms/', include('alarms.urls')),
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
-=======
     path('crawler/', include('crawler.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
->>>>>>> 574b800fcdf6d1fa7d7504dc8fb5e287469603ce
