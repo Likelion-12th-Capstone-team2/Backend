0973bfc045aba41f5e5276d93b8966a7efba232f
minsihihi <126065697+minsihihi@users.noreply.github.com>
2024-12-26 12:25:17 +0900

    Update seleniumCrawl.py

--- crawler/seleniumCrawl.py
+++ crawler/seleniumCrawl.py
@@ -16,7 +16,7 @@ def setup_driver():
     options.add_argument(
         'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     )
-    service = Service(ChromeDriverManager().install())
+    service = Service(ChromeDriverManager(path="/tmp/.wdm").install())
     driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=options)
     return driver
 
