d51848fbda3206d00604c9e1ae28743136215f8b
jimin <010709min@ewhain.net>
2024-12-26 19:22:52 +0900

    fix base.py

--- capstone/settings/base.py
+++ capstone/settings/base.py
@@ -13,13 +13,7 @@ pymysql.install_as_MySQLdb()
 import pymysql
 pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__))))
-
-env = environ.Env(
-    DEBUG=(bool, True)
-)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 env = environ.Env(
     DEBUG=(bool, True)
