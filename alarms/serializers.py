b8f24c4d94ff8ea843f327efd633aa4f0249bad4
soli-ziyo <solac@ewha.ac.kr>
2024-12-28 00:38:34 +0900

    [fix] 알람 시리얼라이저 변경(sender id -> name)

--- alarms/serializers.py
+++ alarms/serializers.py
@@ -3,6 +3,7 @@ from .models import *
 
 # 위시리스트 목록에서 객체를 조회하는 시리얼라이저
 class AlarmSerializer(serializers.ModelSerializer):
+  sender = serializers.SerializerMethodField()  # sender 필드를 커스텀 처리
   class Meta:
     date = serializers.SerializerMethodField()
 
@@ -11,4 +12,7 @@ class AlarmSerializer(serializers.ModelSerializer):
 
     def get_date(self, obj):
         return obj.date.strftime('%Y.%m.%d.')
+    
+    def get_sender(self, obj):
+        return obj.sender.username  # sender의 이름(username)을 반환
 
