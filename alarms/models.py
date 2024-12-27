700f32b20ac3927def59e9fca60ed547b491e99e
soli-ziyo <solac@ewha.ac.kr>
2024-12-28 01:09:41 +0900

    [fix] 알람 시리얼라이저 변경(sender id -> name)

--- alarms/models.py
+++ alarms/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User
 from wish.models import Wish
+from mypage.models import MyPage
 
 # Create your models here.
 
@@ -11,7 +12,9 @@ class Alarm(models.Model):
    item = models.ForeignKey(Wish,on_delete=models.CASCADE)
    
    def sender_name(self):
-        return self.sender.username  # sender가 User 객체로 연결되어 있어야 함
+        # sender와 연결된 MyPage 객체가 있는 경우 name 반환
+        mypage = MyPage.objects.filter(user=self.sender).first()
+        return mypage.name if mypage else "No Name"
    
    def __str__(self):
     return f"{self.sender.username} to {self.receiver.username}"
