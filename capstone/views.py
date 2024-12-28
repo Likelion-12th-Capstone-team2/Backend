47a6fe43890408211b2158738514f278d84590cf
jimin <010709min@ewhain.net>
2024-12-28 12:46:55 +0900

    fix health import 문 수정정

--- capstone/views.py
+++ capstone/views.py
@@ -1,8 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from .models import Alarm
-from .serializers import AlarmSerializer
+
 from rest_framework.status import *
 # Create your views here.
 
